b62cb07294939a4ac26b6216b8c1a3720f993562
HaudeL <haude.lecamus@gmail.com>
2018-02-27 12:07:34 +0100

    es6

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
-app.get('/', function(req, res){
-    res.send("Hello World !");
-});
+
+app.get('/vehicule', (req, res) => {
+    res.send("Oh la belle rouge");
+})
+//app.get('/', (req, res) => {
+   // res.send("Hello World !");
+//});
 app.listen(8080,function(){
     console.log('listening on port 8080');
 });
