4bdef47b58fe52cbee1cb682450e7c845a9d691f
HaudeL <haude.lecamus@gmail.com>
2018-02-28 11:03:21 +0100

    le post marche

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+  });
+
 app.post('/vehicule', (req, res) => {
-    req.body('');
+    res.send('Got a POST request');
   });
 app.listen(3030,function(){
     console.log('listening on port 3030');
 });
+app.get('/recup', (req, res) => {
+    res.send('c écolo !');
+  });
